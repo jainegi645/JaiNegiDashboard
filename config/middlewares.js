20cb9b51c7fe385541ed58f465b4902b057ceba3
jainegi <jainegi645@gmail.com>
2023-02-07 11:58:53 +0530

    aded strapi security middleware in middleware file

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,30 @@
+// image was breakin  while uploading to S3 bucket, so we need to add the `strapi::security` middleware 
+const BUCKET = process.env.AWS_BUCKET_NAME;
+const BUCKET_URL = `https://${BUCKET}.s3.amazonaws.com`;
+
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", BUCKET_URL],
+          "media-src": ["'self'", "data:", "blob:", BUCKET_URL],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', 'ec2-3-109-2-132.ap-south-1.compute.amazonaws.com'),   //ec2-3-109-2-132.ap-south-1.compute.amazonaws.com
+  host: env('HOST', '0.0.0.0'),   //ec2-3-109-2-132.ap-south-1.compute.amazonaws.com
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
