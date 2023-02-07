9c9e3e73bcb7e8affb8f1f45bf57e73e1bd9c558
jainegi <jainegi645@gmail.com>
2023-02-07 12:40:17 +0530

    hide env file from github

--- config/plugins.js
+++ config/plugins.js
@@ -3,8 +3,8 @@ module.exports = ({ env }) => ({
     config: {
         provider: 'aws-s3',
         providerOptions: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID','AKIAW53ZRXDKGBETOKP3'),
-          secretAccessKey: env('AWS_ACCESS_SECRET','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'),
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_ACCESS_SECRET'),
           region: env('AWS_REGION','ap-south-1'),
           params: {
               Bucket: env('AWS_BUCKET_NAME','jainegiporfoliobucket'),
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,27 @@
-module.exports = {
-    apps: [
-      {
-        name: 'JaiNegiDashboard', // Your project name
-        cwd: '/home/ubuntu/JaiNegiDashboard', // Path to your project
-        script: 'npm', // For this example we're using npm, could also be yarn
-        args: 'start', // Script to start the Strapi server, `start` by default
-        env: {
-          APP_KEYS:'IMmdfGPbNhBhSFkPEFuHmw==,cGOHikgm2d+oQlAgH9pC9A==,iWAE1Boh/is8ASulQYRabQ==,F9g5Zf0JD4VmYYReSpMBYw==',
-          API_TOKEN_SALT: 'BAgoO+iLQ8ZIzmawSmviwg==',
-          ADMIN_JWT_SECRET: 'nxxHSC5aeEkr0j7yC/lq0A==',
-          JWT_SECRET: 'kdaM0Yn0zmUhZSawg4dccg==',
-          NODE_ENV: 'production',
-          DATABASE_HOST: 'porfoliodashboarddb.ctkmvzgif0cy.ap-south-1.rds.amazonaws.com', // database Endpoint under 'Connectivity & Security' tab
-          DATABASE_PORT: '5432',
-          DATABASE_NAME: 'postgres', // DB name under 'Configuration' tab
-          DATABASE_USERNAME: 'postgres', // default username
-          DATABASE_PASSWORD: 'Negijai1234',
-          AWS_ACCESS_KEY_ID: 'AKIAW53ZRXDKGBETOKP3',
-          AWS_ACCESS_SECRET: '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', // Find it in Amazon S3 Dashboard
-          AWS_REGION: 'ap-south-1',
-          AWS_BUCKET_NAME: 'jainegiporfoliobucket',
-        },
+module.exports = ({ env }) => ({
+  apps: [
+    {
+      name: "JaiNegiDashboard", // Your project name
+      cwd: "/home/ubuntu/JaiNegiDashboard", // Path to your project
+      script: "npm", // For this example we're using npm, could also be yarn
+      args: "start", // Script to start the Strapi server, `start` by default
+      env: {
+        APP_KEYS:
+          "IMmdfGPbNhBhSFkPEFuHmw==,cGOHikgm2d+oQlAgH9pC9A==,iWAE1Boh/is8ASulQYRabQ==,F9g5Zf0JD4VmYYReSpMBYw==",
+        API_TOKEN_SALT: env("API_TOKEN_SALT="),
+        ADMIN_JWT_SECRET: env("ADMIN_JWT_SECRET"),
+        JWT_SECRET: env("JWT_SECRET"),
+        NODE_ENV: "production",
+        DATABASE_HOST: env("DATABASE_HOST"), // database Endpoint under 'Connectivity & Security' tab
+        DATABASE_PORT: env("DATABASE_PORT"),
+        DATABASE_NAME: env("DATABASE_NAME"), // DB name under 'Configuration' tab
+        DATABASE_USERNAME: env("DATABASE_USERNAME"), // default username
+        DATABASE_PASSWORD: env("DATABASE_PASSWORD"),
+        AWS_ACCESS_KEY_ID: env("AWS_ACCESS_KEY_ID"),
+        AWS_ACCESS_SECRET: env("AWS_ACCESS_SECRET"), // Find it in Amazon S3 Dashboard
+        AWS_REGION: env("AWS_REGION"),
+        AWS_BUCKET_NAME: env("AWS_BUCKET_NAME"),
       },
-    ],
-  };
+    },
+  ],
+});
